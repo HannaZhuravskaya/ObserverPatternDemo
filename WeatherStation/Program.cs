7cf01e50e85113e6adcc6ec6e9b0a4ea896a9f15
HannaZhuravskaya <hanna.zhuravskaya@gmail.com>
2018-11-05 01:23:19 +0300

    Added settings to project and console tests to class Program.

--- WeatherStation/Program.cs
+++ WeatherStation/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace WeatherStation
+﻿using System;
+using ObserverPatternDemo.Implemantation.Observable;
+using ObserverPatternDemo.Implemantation.Observers;
+
+namespace WeatherStation
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var weatherData = new WeatherData();
+            var currentConditionsReport = new CurrentConditionsReport();
+            var statisticsReport = new StatisticReport();
+            weatherData.Register(currentConditionsReport);
+            weatherData.Register(statisticsReport);
+
+            var weatherInfo = new WeatherInfo(){Temperature = 10, Humidity = 15, Pressure = 30};
+            weatherData.UpdateWeatherInfo(weatherInfo);
+
+            Console.WriteLine(currentConditionsReport.MakeReport() + "\n\n");
+            Console.WriteLine(statisticsReport.MakeReport() + "\n\n");
+
+            weatherInfo.Temperature = -10;
+            weatherInfo.Humidity = 3;
+            weatherInfo.Pressure = 87;
+            weatherData.UpdateWeatherInfo(weatherInfo);
+
+            Console.WriteLine(currentConditionsReport.MakeReport() + "\n\n");
+            Console.WriteLine(statisticsReport.MakeReport() + "\n\n");
         }
     }
 }
